864499dc41a02039f11e1a8f5f7dc278a8fcd79a
Maslin Farrell <maslinfar@gmail.com>
2023-01-05 16:21:00 -0800

    Updated express server to handle search function

--- index.js
+++ index.js
@@ -1,7 +1,64 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+let requestedProvider = "";
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.post('/requestProvider', (req, res) =>{
+    requestedProvider = req.body.providerCommand;
+    if(requestedProvider == "!yt"){
+        const message = {
+            'Name': 'YouTube',
+            'URL': 'https://www.youtube.com/results?search_query=',
+        };
+        return res.send(JSON.stringify(message));
+    }else if(requestedProvider == "!wk"){
+        const message = {
+            'Name': 'Wikipedia',
+            'URL': 'https://en.wikipedia.org/wiki/',
+        };
+        return res.send(JSON.stringify(message));
+    }else if(requestedProvider == "!am"){
+        const message = {
+            'Name': 'Amazon',
+            'URL': 'https://www.amazon.com/s?k=',
+        };
+        return res.send(JSON.stringify(message));
+    }else if(requestedProvider == "!r"){
+        const message = {
+            'Name': 'Reddit',
+            'URL': 'https://www.reddit.com/search/?q=',
+        };
+        return res.send(JSON.stringify(message));
+    }else if(requestedProvider == "!g"){
+        const message = {
+            'Name': 'Search',
+            'URL': 'https://www.google.com/search?q=',
+        };
+        return res.send(JSON.stringify(message));
+    }else if(requestedProvider == "!dc"){
+        const message = {
+            'Name': 'Discogs',
+            'URL': 'https://www.discogs.com/search/?q=',
+        };
+        return res.send(JSON.stringify(message));
+    }else{
+        const message = {
+            'Name': 'Search',
+            'URL': 'https://www.google.com/search?q=',
+        };
+        return res.send(JSON.stringify(message));
+    }
+    
+});
+
 
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)

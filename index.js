513e8b0ba65d3ce42a7c1f2bdf9c6cbf29041d06
Maslin Farrell <maslinfar@gmail.com>
2023-01-06 16:29:33 -0800

    Reorderd executions

--- index.js
+++ index.js
@@ -9,54 +9,47 @@ const PORT = 3000;
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+ 
+let bongoGif;
+let bookmarks = "";
+let backgroundURL = "";
+let searchProvider = "";
 
-async function main(){
-    let bongoGif;
-    let bookmarks = "";
-    let backgroundURL = "";
-    let searchProvider = "";
-    
-    const fetchUpdate = async() =>{
-        console.log("Fetching");
-        const res = await fetch('https://raw.githubusercontent.com/maslindc2/startpage-cms/main/List.json').then(response => response.json()).then(json => {
-            bongoGif = json.BongoGif;
-            bookmarks = json.Bookmarks;
-            backgroundURL = json.Background;
-            searchProvider = json.SearchProviders;
-        });
-    }
-    
-    app.listen(PORT, () => {
-        console.log(`Listening on PORT ${PORT}`);
-        
-        //On startup call fetchUpdate to populate the variables
-        fetchUpdate();
+const fetchUpdate = async() =>{
+    console.log("Fetching");
+    const res = await fetch('https://raw.githubusercontent.com/maslindc2/startpage-cms/main/List.json').then(response => response.json()).then(json => {
+        bongoGif = json.BongoGif;
+        bookmarks = json.Bookmarks;
+        backgroundURL = json.Background;
+        searchProvider = json.SearchProviders;
     });
-
-    app.post('/update', (req, res) =>{
-        updateRequest = req.body.Command;
-        console.log("Received request update");
-        fetchUpdate();
-    });
-
-    app.get('/bongoGif', (req, res) =>{
-        res.send(bongoGif);
-    });
-    
-    app.get('/bookmarks', (req, res) =>{
-        res.send(bookmarks);
-    });
-
-    app.get('/background', (req, res) => {
-        res.send(backgroundURL);
-    });
-    
-    app.get('/searchProvider', (req, res) =>{
-        res.send(searchProvider);
-    });
-    
-    
-    module.exports = app;
-    
 }
-main();
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+    
+    //On startup call fetchUpdate to populate the variables
+    fetchUpdate();
+});
+app.post('/update', (req, res) =>{
+    updateRequest = req.body.Command;
+    console.log("Received request update");
+    fetchUpdate();
+});
+app.get('/bongoGif', (req, res) =>{
+    res.send(bongoGif);
+});
+
+app.get('/bookmarks', (req, res) =>{
+    res.send(bookmarks);
+});
+app.get('/background', (req, res) => {
+    res.send(backgroundURL);
+});
+
+app.get('/searchProvider', (req, res) =>{
+    res.send(searchProvider);
+});
+
+
+module.exports = app;
